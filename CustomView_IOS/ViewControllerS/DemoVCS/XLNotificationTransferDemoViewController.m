30c11e0ca1ef0b3b29c8c8f79c5b73c16f8adf94
恒善信诚科技有限公司 <515521265@qq.com>
2017-01-17 14:10:41 +0800

    通知处理。

--- CustomView_IOS/ViewControllerS/DemoVCS/XLNotificationTransferDemoViewController.m
+++ CustomView_IOS/ViewControllerS/DemoVCS/XLNotificationTransferDemoViewController.m
@@ -15,27 +15,9 @@
 
 @interface XLNotificationTransferDemoViewController ()
 
-@property (nonatomic,strong) JWScrollView * scrollView;
-
-@property (nonatomic,strong) NSMutableArray <LayoutModel *>* modelArr;
-
 @end
 
 @implementation XLNotificationTransferDemoViewController
-
--(JWScrollView *)scrollView{
-    
-    if (!_scrollView) {
-        _scrollView = [[JWScrollView alloc]init];
-        _scrollView.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight);
-        _scrollView.alwaysBounceVertical = true;
-//        _scrollView.delaysContentTouches =false;
-        [self.view addSubview:_scrollView];
-    }
-    return _scrollView;
-}
-
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -50,69 +32,7 @@
         NSLog(@"----showkeyboard");
     }];
     
-    self.modelArr = [NSMutableArray arrayWithCapacity:10];
-    
-    for (int i =  0; i< 13; i++) {
-        LayoutModel * model  = [LayoutModel new];
-        model.title = @"包包";
-        model.pid = i+1;
-        model.image = [NSString stringWithFormat:@"common-hud%d",i+1];
-        [self.modelArr addObject:model];
-    }
-    
-    for (int i = 0 ; i < self.modelArr.count; i++){
-        UIButton *button = [self CreateBtnWithTitle:self.modelArr[i].title imageStr:self.modelArr[i].image];
-        button.tag = 1001+i;
-        NSInteger a = i / 3;
-        NSInteger b = i % 3;
-        button.x = b * kScreenWidth / 3;
-        button.y = (a * kScreenWidth / 3) +64;
-        [self.scrollView addSubview:button];
-        self.scrollView.contentSize = CGSizeMake(0, CGRectGetMaxY(button.frame)+10);
-    }
-    
 }
-
--(UIButton *)CreateBtnWithTitle:(NSString *)title imageStr:(NSString *)imageStr{
-
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    button.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    [button setTitleColor:UIColorFromRGB(0x656364) forState:UIControlStateNormal];
-    [button setBackgroundImage:[UIImage imageWithColor:UIColorFromRGB(0xffffff)] forState:UIControlStateNormal];
-    [button setBackgroundImage:[UIImage imageWithColor:UIColorFromRGB(0xf4f4f4)] forState:UIControlStateHighlighted];
-    button.layer.borderWidth = 0.5;
-    button.layer.borderColor = UIColorFromRGB(0xefeaea).CGColor;
-    [button addTarget:self action:@selector(buttonitem:) forControlEvents:UIControlEventTouchUpInside];
-    button.frame = CGRectMake( 0, 0, kScreenWidth / 3, kScreenWidth / 3);
-    UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(button.width/2-25, 20, 50, 50)];
-    imageView.image = [UIImage imageNamed:imageStr];
-    [button addSubview:imageView];
-    UILabel * titleLab = [[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(imageView.frame), button.width, 40)];
-    titleLab.text = title;
-    titleLab.textAlignment = 1;
-    titleLab.textColor = UIColorFromRGB(0x535353);
-    titleLab.font = [UIFont systemFontOfSize:13];
-    [button addSubview:titleLab];
-    return button;
-}
-
--(void)buttonitem:(UIButton *)btn{
-    
-    Class availableClass = NSClassFromString(self.modelArr[btn.tag - 1001].clasStr);
-    if (availableClass) {
-        BaseViewController * viewController = [NSClassFromString(self.modelArr[btn.tag - 1001].clasStr) new];
-        [self.navigationController pushViewController:viewController animated:true];
-    }else{
-    #ifdef DEBUG
-            [TooltipView showWithText:self.modelArr[btn.tag - 1001].clasStr];
-    #else
-    #endif
-    }
-
-    
-    
-}
-
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [@"tongzhi" postNotification_userInfo:@"haha"];
