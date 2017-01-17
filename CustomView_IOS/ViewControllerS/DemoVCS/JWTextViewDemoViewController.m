d02719b06362cad5b0f6771c62398ffab4643e95
恒善信诚科技有限公司 <515521265@qq.com>
2017-01-17 11:56:08 +0800

    fix bug.

--- CustomView_IOS/ViewControllerS/DemoVCS/JWTextViewDemoViewController.m
+++ CustomView_IOS/ViewControllerS/DemoVCS/JWTextViewDemoViewController.m
@@ -65,6 +65,7 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 
         cell2.contentView.height = 100;
+        [cell2 refreshSubviews];
         
     });
 
