67a840321363e23a752ece1c0c86906f5c4ced13
恒善信诚科技有限公司 <515521265@qq.com>
2017-04-12 10:07:49 +0800

    按钮自定义布局。

--- CustomView_IOS/ViewControllerS/DemoVCS/SpinnerDemoViewController.m
+++ CustomView_IOS/ViewControllerS/DemoVCS/SpinnerDemoViewController.m
@@ -15,6 +15,8 @@
 
 #import "MVMaterialView.h"
 
+#import "UIButton+Layout.h"
+
 //#import "helloC.h"
 
 @interface SpinnerDemoViewController ()<SpinnerViewDelegate>
@@ -88,6 +90,34 @@
             [button setTitle:backStr forState:UIControlStateNormal];
         };
     }];
+    
+    
+    
+    UIButton * searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [searchBtn setImage:[UIImage imageNamed:@"arrow_forward"] forState:UIControlStateNormal];
+    [searchBtn setTitle:@"搜索按钮图片在左边" forState:UIControlStateNormal];
+    searchBtn.titleLabel.font = [UIFont systemFontOfSize:13];
+    [searchBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [searchBtn setTitleColor:[UIColor orangeColor] forState:UIControlStateHighlighted];
+    searchBtn.imageRect = CGRectMake(10, 10, 20, 20);
+    searchBtn.titleRect = CGRectMake(35, 10, 120, 20);
+    [self.view addSubview:searchBtn];
+    searchBtn.frame = CGRectMake(kScreenWidth * 0.5 - 80, 250, 160, 40);
+    searchBtn.backgroundColor = [UIColor colorWithRed:255/255.0 green:242/255.0 blue:210/255.0 alpha:1];
+    
+    
+    
+    UIButton * cancelBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [cancelBtn setImage:[UIImage imageNamed:@"arrow_forward"] forState:UIControlStateNormal];
+    [cancelBtn setTitle:@"取消按钮图片在右边" forState:UIControlStateNormal];
+    cancelBtn.titleLabel.font = [UIFont systemFontOfSize:13];
+    [cancelBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:[UIColor orangeColor] forState:UIControlStateHighlighted];
+    cancelBtn.titleRect = CGRectMake(10, 10, 120, 20);
+    cancelBtn.imageRect = CGRectMake(135, 10, 20, 20);
+    [self.view addSubview:cancelBtn];
+    cancelBtn.frame = CGRectMake(kScreenWidth * 0.5 - 80, 350, 160, 40);
+    cancelBtn.backgroundColor = [UIColor colorWithRed:255/255.0 green:242/255.0 blue:210/255.0 alpha:1];
     
 }
 
