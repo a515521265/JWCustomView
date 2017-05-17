2a610f3e244ef9a57cb147fb9e9674290b12f292
梁家文 <515521265@qq.com>
2017-05-17 10:01:27 +0800

    添加拆分数组方法。

--- CustomView_IOS/AppDelegate.m
+++ CustomView_IOS/AppDelegate.m
@@ -21,6 +21,7 @@
 #import "JxbDebugTool.h"
 
 #import "KTouchPointerWindow.h"
+#import "NSObject+Extension.h"
 
 @interface AppDelegate ()
 @property (nonatomic,strong) FPSLabelManager * showFPSLabel;
@@ -62,10 +63,10 @@
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     
-    
-    [[JxbDebugTool shareInstance] setMainColor:[UIColor redColor]];
-    [[JxbDebugTool shareInstance] enableDebugMode];
-    
+//    
+//    [[JxbDebugTool shareInstance] setMainColor:[UIColor redColor]];
+//    [[JxbDebugTool shareInstance] enableDebugMode];
+//    
     
     //fpslab
     FPSLabelManager * fpsL = [FPSLabelManager new];
@@ -230,17 +231,101 @@
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
     
-    NSString *urlString = [url absoluteString];
-    if ([urlString isEqualToString:@"hsxc://starapp/index"]) {
-
-        NSLog(@"链接进入app");
+    NSString *urll = url.absoluteString;
+    NSString *scheme = @"hsxc://";
+    if ([urll hasPrefix:scheme]) {
+//        [self getURLParameters:urll];
+        NSLog(@"%@",[self getURLParameters:urll]);
         
-        return YES;
+        return NO;
+    }
+    return YES;
+    
+//    hsxc://platformapi/startapp?saId=10000007&qrcode=HTTPS://QR.ALIPAY.COM/FKX05817EWSX8XRYV7SW28
+}
+/**
+ *  截取URL中的参数
+ *
+ *  @return NSMutableDictionary parameters
+ */
+- (NSMutableDictionary *)getURLParameters:(NSString *)urlStr {
+    
+    // 查找参数
+    NSRange range = [urlStr rangeOfString:@"?"];
+    if (range.location == NSNotFound) {
+        return nil;
     }
     
-    return YES;
+    // 以字典形式将参数返回
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    
+    // 截取参数
+    NSString *parametersString = [urlStr substringFromIndex:range.location + 1];
+    
+    // 判断参数是单个参数还是多个参数
+    if ([parametersString containsString:@"&"]) {
+        
+        // 多个参数，分割参数
+        NSArray *urlComponents = [parametersString componentsSeparatedByString:@"&"];
+        
+        for (NSString *keyValuePair in urlComponents) {
+            // 生成Key/Value
+            NSArray *pairComponents = [keyValuePair componentsSeparatedByString:@"="];
+            NSString *key = [pairComponents.firstObject stringByRemovingPercentEncoding];
+            NSString *value = [pairComponents.lastObject stringByRemovingPercentEncoding];
+            
+            // Key不能为nil
+            if (key == nil || value == nil) {
+                continue;
+            }
+            
+            id existValue = [params valueForKey:key];
+            
+            if (existValue != nil) {
+                
+                // 已存在的值，生成数组
+                if ([existValue isKindOfClass:[NSArray class]]) {
+                    // 已存在的值生成数组
+                    NSMutableArray *items = [NSMutableArray arrayWithArray:existValue];
+                    [items addObject:value];
+                    
+                    [params setValue:items forKey:key];
+                } else {
+                    
+                    // 非数组
+                    [params setValue:@[existValue, value] forKey:key];
+                }
+                
+            } else {
+                
+                // 设置值
+                [params setValue:value forKey:key];
+            }
+        }
+    } else {
+        // 单个参数
+        
+        // 生成Key/Value
+        NSArray *pairComponents = [parametersString componentsSeparatedByString:@"="];
+        
+        // 只有一个参数，没有值
+        if (pairComponents.count == 1) {
+            return nil;
+        }
+        
+        // 分隔值
+        NSString *key = [pairComponents.firstObject stringByRemovingPercentEncoding];
+        NSString *value = [pairComponents.lastObject stringByRemovingPercentEncoding];
+        
+        // Key不能为nil
+        if (key == nil || value == nil) {
+            return nil;
+        }
+        
+        // 设置值
+        [params setValue:value forKey:key];
+    }
+    
+    return params;
 }
-
-
-
 @end
--- CustomView_IOS/ViewControllerS/DemoListViewController.m
+++ CustomView_IOS/ViewControllerS/DemoListViewController.m
@@ -88,9 +88,46 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         dismissHUD();
     });
+    
+    
+    NSMutableArray * arr = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10"].mutableCopy;
+    NSArray * allSubArr = [self splitArray:arr withSubSize:3];
+    
+    NSLog(@"数据---%@",allSubArr);
+    
 
-    
-    
+}
+
+
+/**
+ *  拆分数组
+ *  @param array   需要拆分的数组
+ *  @param subSize 指定长度
+ *  @return 包含子数组的数组
+ */
+- (NSArray *)splitArray: (NSArray *)array withSubSize : (int)subSize{
+    //  数组将被拆分成指定长度数组的个数
+    unsigned long count = array.count % subSize == 0 ? (array.count / subSize) : (array.count / subSize + 1);
+    //  用来保存指定长度数组的可变数组对象
+    NSMutableArray *arr = [[NSMutableArray alloc] init];
+    //利用总个数进行循环，将指定长度的元素加入数组
+    for (int i = 0; i < count; i ++) {
+        //数组下标
+        int index = i * subSize;
+        //保存拆分的固定长度的数组元素的可变数组
+        NSMutableArray *arr1 = [[NSMutableArray alloc] init];
+        //移除子数组的所有元素
+        [arr1 removeAllObjects];
+        int j = index;
+        //将数组下标乘以1、2、3，得到拆分时数组的最大下标值，但最大不能超过数组的总大小
+        while (j < subSize*(i + 1) && j < array.count) {
+            [arr1 addObject:[array objectAtIndex:j]];
+            j += 1;
+        }
+        //将子数组添加到保存子数组的数组中
+        [arr addObject:[arr1 copy]];
+    }
+    return [arr copy];
 }
 
 
